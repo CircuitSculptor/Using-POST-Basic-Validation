bc883083e8e8760f14051d2e19071d674cd7c0c3
Bartlomiej Drapinski <g00428194@atu.ie>
2025-09-28 15:54:19 +0100

    Setting up project

---
+++ src/main/java/ie/atu/week3sem1/usingpost_basicvalidation/UsingPostBasicValidationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3sem1.usingpost_basicvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingPostBasicValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingPostBasicValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3sem1/usingpost_basicvalidation/UsingPostBasicValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3sem1.usingpost_basicvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingPostBasicValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
