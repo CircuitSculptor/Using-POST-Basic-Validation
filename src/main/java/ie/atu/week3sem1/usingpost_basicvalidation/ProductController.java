804903e4d0c840a38b9be09c97817b9267635481
Bartlomiej Drapinski <g00428194@atu.ie>
2025-10-01 15:00:49 +0100

    Added supporting text to getCount

--- src/main/java/ie/atu/week3sem1/usingpost_basicvalidation/ProductController.java
+++ src/main/java/ie/atu/week3sem1/usingpost_basicvalidation/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController {
 
     @GetMapping("/getCount")
     public int getCount() {
-        return myList.size();
+        return Integer.parseInt("Amount of products added = " + myList.size());
     }
 
     @PostMapping("/addProduct")
