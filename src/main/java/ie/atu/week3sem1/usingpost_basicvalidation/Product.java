e3f00f8543e04897d5417c57f49cad0afdb1bb72
Bartlomiej Drapinski <g00428194@atu.ie>
2025-10-01 14:43:19 +0100

    Added Validation for product name size

--- src/main/java/ie/atu/week3sem1/usingpost_basicvalidation/Product.java
+++ src/main/java/ie/atu/week3sem1/usingpost_basicvalidation/Product.java
@@ -2,6 +2,7 @@ package ie.atu.week3sem1.usingpost_basicvalidation;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class Product {
     @NotBlank
+    @Size(min = 3, max = 20)
     private String productName;
     @Positive
     private double price;
